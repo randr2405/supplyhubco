bc18f4a0f525ebef74cd3594ad147ed2119a494e
Rhea0524 <ST10264009@vcconnect.edu.za>
2026-02-05 12:24:16 +0200

    Sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://supplyhubco.co.za',
+  generateRobotsTxt: true, // This also creates robots.txt
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 5000,
+}
